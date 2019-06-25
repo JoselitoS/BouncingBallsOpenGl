ff5168859e7995faf66a7798db136689bc32d7fe
JoselitoS <gabriel.serrano@pasoglobal.com>
2019-06-25 16:59:00 +0200

    BouncingBallsConfig.h

--- BouncingBallsConfig.h
+++
@@ -1,3 +0,0 @@
-// the configured options and settings for BouncingBallls
-#define BouncingBalls_VERSION_MAJOR @BouncingBalls_VERSION_MAJOR@
-#define BouncingBalls_VERSION_MINOR @BouncingBalls_VERSION_MINOR@
